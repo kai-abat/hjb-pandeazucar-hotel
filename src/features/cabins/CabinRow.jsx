7c34ee6d2c587f55c5e5332e1792bd51e622c8b9
Kai <kai.balsamo.abat@gmail.com>
2023-12-10 22:57:39 +0800

    Implement compound component to Table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,18 +6,19 @@ import { formatCurrency } from "../../utils/helpers";
 import { useDeleteCabins } from "./useDeleteCabins";
 import { useCreateCabins } from "./useCreateCabins";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -73,7 +74,7 @@ function CabinRow({ cabin }) {
 
   return (
     <>
-      <TableRow>
+      <Table.Row>
         <Img src={image} alt={name} />
         <Cabin>{name}</Cabin>
         <Price>{regularPrice}</Price>
@@ -119,7 +120,7 @@ function CabinRow({ cabin }) {
             <HiTrash />
           </button> */}
         </div>
-      </TableRow>
+      </Table.Row>
       {/* {showForm && <CreateCabinForm cabinToEdit={cabin} />} */}
     </>
   );
