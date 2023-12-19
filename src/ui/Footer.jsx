90bad15922f55f59377213be094fc8ad36debe8f
Kai <kai.balsamo.abat@gmail.com>
2023-12-19 16:56:13 +0800

    fix bug small mobile device of footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -10,7 +10,7 @@ const StyledFooter = styled.div`
   justify-content: space-between;
   padding: 2rem 4rem 4rem 4rem;
   border-top: 1px solid var(--color-grey-100);
-  @media ${DEVICE_MAX_W.mobileS} {
+  @media ${DEVICE_MAX_W.mobileM2} {
     flex-direction: column;
     justify-content: flex-start;
     gap: 2rem;
